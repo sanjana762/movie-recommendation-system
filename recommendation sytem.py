6dfa55607c26e201a938960bb3093f1e136fc78a
sanjana762 <142682250+sanjana762@users.noreply.github.com>
2024-07-15 19:55:10 +0530

    Update recommendation sytem.py

--- recommendation sytem.py
+++ recommendation sytem.py
@@ -3,3 +3,105 @@ import numpy as np
 df1=pd.read_csv('/kaggle/input/movie-recommendation-system/tmdb_5000_credits.csv')
 df2=pd.read_csv('/kaggle/input/movie-recommendation-system/tmdb_5000_movies.csv')
 
+df1.columns = ['id','tittle','cast','crew']
+df2= df2.merge(df1,on='id')
+
+df2.head(5)
+C= df2['vote_average'].mean()
+C
+m= df2['vote_count'].quantile(0.9)
+m
+q_movies = df2.copy().loc[df2['vote_count'] >= m]
+q_movies.shape
+def weighted_rating(x, m=m, C=C):
+    v = x['vote_count']
+    R = x['vote_average']
+    # Calculation based on the IMDB formula
+    return (v/(v+m) * R) + (m/(m+v) * C)
+  # Define a new feature 'score' and calculate its value with `weighted_rating()`
+q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
+#Sort movies based on score calculated above
+q_movies = q_movies.sort_values('score', ascending=False)
+
+#Print the top 15 movies
+q_movies[['title', 'vote_count', 'vote_average', 'score']].head(10)
+
+pop= df2.sort_values('popularity', ascending=False)
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12,4))
+
+plt.barh(pop['title'].head(6),pop['popularity'].head(6), align='center',
+        color='skyblue')
+plt.gca().invert_yaxis()
+plt.xlabel("Popularity")
+plt.title("Popular Movies")
+
+df2['overview'].head(5)
+
+#Import TfIdfVectorizer from scikit-learn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf = TfidfVectorizer(stop_words='english')
+
+#Replace NaN with an empty string
+df2['overview'] = df2['overview'].fillna('')
+
+#Construct the required TF-IDF matrix by fitting and transforming the data
+tfidf_matrix = tfidf.fit_transform(df2['overview'])
+
+#Output the shape of tfidf_matrix
+tfidf_matrix.shape
+
+# Import linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
+
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+#Construct a reverse map of indices and movie titles
+indices = pd.Series(df2.index, index=df2['title']).drop_duplicates()
+# Function that takes in movie title as input and outputs most similar movies
+def get_recommendations(title, cosine_sim=cosine_sim):
+    # Get the index of the movie that matches the title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return df2['title'].iloc[movie_indices]
+  
+  get_recommendations('The Dark Knight Rises')
+
+get_recommendations('The Avengers')
+
+from surprise import Reader, Dataset, SVD, evaluate
+reader = Reader()
+ratings = pd.read_csv('../input/the-movies-dataset/ratings_small.csv')
+ratings.head()
+
+data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+data.split(n_folds=5)
+
+svd = SVD()
+evaluate(svd, data, measures=['RMSE', 'MAE'])
+
+trainset = data.build_full_trainset()
+svd.fit(trainset)
+
+ratings[ratings['userId'] == 1]
+
+svd.predict(1, 302, 3)
+
+
+  
+
